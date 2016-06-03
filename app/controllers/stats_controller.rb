3ccdd50466ca398c3b97d8c07299dde5e4a1a4b5
John Otander <johnotander@gmail.com>
2016-06-03 09:30:52 -0600

    Add more data to the stats endpoint

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,10 +1,16 @@
 class StatsController < ApplicationController
   def index
+    votes_count = Vote.count
+    combos_count = Combo.count
+
     render json: {
-      combos: Combo.count,
-      votes: Vote.count,
+      combos: combos_count,
+      votes: votes_count,
+      votes_per_combo: combos_count / votes_count.to_f,
       up_votes: Vote.where(value: true).count,
-      down_votes: Vote.where(value: false).count
+      down_votes: Vote.where(value: false).count,
+      latest_20: Combo.all.order(created_at: :desc).limit(20),
+      most_active_20: Combo.all.order(votes_count: :desc).limit(20)
     }
   end
 end
